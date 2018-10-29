f4449b1a3159497349aeb8c441bef3c334559239
Jose Ronaldo <jronaldo.ti@gmail.com>
2018-10-29 10:46:07 -0300

    Adcionando Login logout inicial

--- Sigest/SigestAlfa/settings.py
+++ Sigest/SigestAlfa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'funcoes',
     'usuarios',
     'tarefas',
+    'contas'
 
 ]
 
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
